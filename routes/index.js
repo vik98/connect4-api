0ffe1ef5e1c48fe8c6c1e46c0cd0a55b533e2d78
Vikramsingh Rathod <rathodvikram44@gmail.com>
2020-09-27 22:19:54 +0530

    add the logic for updation

--- routes/index.js
+++ routes/index.js
@@ -18,61 +18,80 @@ router.get("/startNewGame", function (req, res, next) {
     .catch((err) => console.log(err));
 });
 
+router.get("/getGame", function (req, res, next) {
+  res.render("getGame");
+});
+
+router.get("/:id", async function (req, res, next) {
+  const game = await Game.findById(req.params.id).exec();
+  res.json(game);
+});
+
 router.post("/:id/:columnNumber", async function (req, res, next) {
-  //console.log(req.params.id);
   const game = await Game.findById(req.params.id).exec();
   const updatedGame = JSON.parse(JSON.stringify(game));
-  // if (game.moves.length == 0) {
-  //   updatedGame = new Game({
-  //     currentState: game.currentState,
-  //     moves: [
-  //       {
-  //         color: "Yellow",
-  //         moveCount: 1,
-  //         columnNumber: req.params.columnNumber,
-  //       },
-  //     ],
-  //     validMove: game.validMove,
-  //     winner: game.winner,
-  //     gameOver: game.gameOver,
-  //   });
-  //   console.log(updatedGame);
-  // } else {
-  // }
 
+  console.log(game.moves.length);
   if (game.gameOver) {
     updatedGame.comments = "Game is already over. Check the winner attribute";
   } else {
     if (game.currentState[0][req.params.columnNumber - 1] == 0) {
       updatedGame.validMove = true;
       updatedGame.comments =
-        "Valid move. Please check the currentState attribute";
+        "Valid move. Please check the currentState and moves attribute";
+      if (game.moves.length == 0) {
+        updatedGame.moves[0] = {
+          color: "Yellow",
+          moveCount: 1,
+          columnNumber: req.params.columnNumber,
+        };
+        for (let i = 5; i >= 0; i--) {
+          if (game.currentState[i][req.params.columnNumber - 1] == 0) {
+            updatedGame.currentState[i][req.params.columnNumber - 1] = "Y";
+            break;
+          }
+        }
+      } else {
+        if ((updatedGame.moves.length + 1) % 2 == 0) {
+          updatedGame.moves.push({
+            color: "Red",
+            moveCount: updatedGame.moves.length + 1,
+            columnNumber: req.params.columnNumber,
+          });
+          for (let i = 5; i >= 0; i--) {
+            if (game.currentState[i][req.params.columnNumber - 1] == 0) {
+              updatedGame.currentState[i][req.params.columnNumber - 1] = "R";
+              break;
+            }
+          }
+        } else {
+          updatedGame.moves.push({
+            color: "Yellow",
+            moveCount: updatedGame.moves.length + 1,
+            columnNumber: req.params.columnNumber,
+          });
+          for (let i = 5; i >= 0; i--) {
+            if (game.currentState[i][req.params.columnNumber - 1] == 0) {
+              updatedGame.currentState[i][req.params.columnNumber - 1] = "Y";
+              break;
+            }
+          }
+        }
+      }
     } else {
       updatedGame.validMove = false;
       updatedGame.comments = "Invalid move. Please make a valid move";
     }
   }
   console.log(updatedGame);
-  // Game.findById(req.params.id, function (err, game) {
-  //   if (err) console.log(err);
-  //   //console.log(game.moves.length);
-  //   if (game.moves.length == 0) {
-  //     const updatedGame = new Game({
-  //       currentState: game.currentState,
-  //       moves: [
-  //         {
-  //           color: "Yellow",
-  //           moveCount: 1,
-  //           columnNumber: req.params.columnNumber,
-  //         },
-  //       ],
-  //       validMove: game.validMove,
-  //       winner: game.winner,
-  //       gameOver: game.gameOver,
-  //     });
-  //     console.log(updatedGame);
-  //   }
-  // });
+
+  Game.findByIdAndUpdate(req.params.id, updatedGame, { new: true }, function (
+    err,
+    resultGame
+  ) {
+    if (err) console.log(err);
+    res.send(resultGame);
+  });
 });
 
 module.exports = router;
