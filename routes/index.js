af6eef5c90916cc45d46ec6e692adde3a3cc872a
Vikramsingh Rathod <rathodvikram44@gmail.com>
2020-09-27 23:37:21 +0530

    added error handling

--- routes/index.js
+++ routes/index.js
@@ -23,110 +23,118 @@ router.get("/getGame", function (req, res, next) {
 });
 
 router.get("/:id", async function (req, res, next) {
-  const game = await Game.findById(req.params.id).exec();
-  res.json(game);
+  try {
+    const game = await Game.findById(req.params.id).exec();
+    res.json(game);
+  } catch (err) {
+    next(err);
+  }
 });
 
 router.post("/:id/:columnNumber", async function (req, res, next) {
-  const game = await Game.findById(req.params.id).exec();
-  const updatedGame = JSON.parse(JSON.stringify(game));
+  try {
+    const game = await Game.findById(req.params.id).exec();
+    const updatedGame = JSON.parse(JSON.stringify(game));
 
-  console.log(game.moves.length);
-  if (game.gameOver) {
-    updatedGame.comments = "Game is already over. Check the winner attribute";
-  } else {
-    if (game.currentState[0][req.params.columnNumber - 1] == 0) {
-      updatedGame.validMove = true;
-      updatedGame.comments =
-        "Valid move. Please check the currentState and moves attribute";
-      if (game.moves.length == 0) {
-        updatedGame.moves[0] = {
-          color: "YELLOW",
-          moveCount: 1,
-          columnNumber: req.params.columnNumber,
-        };
-        for (let i = 5; i >= 0; i--) {
-          if (game.currentState[i][req.params.columnNumber - 1] == 0) {
-            updatedGame.currentState[i][req.params.columnNumber - 1] = "Y";
-            break;
-          }
-        }
-      } else {
-        if ((updatedGame.moves.length + 1) % 2 == 0) {
-          updatedGame.moves.push({
-            color: "RED",
-            moveCount: updatedGame.moves.length + 1,
-            columnNumber: req.params.columnNumber,
-          });
-          for (let i = 5; i >= 0; i--) {
-            if (game.currentState[i][req.params.columnNumber - 1] == 0) {
-              updatedGame.currentState[i][req.params.columnNumber - 1] = "R";
-              break;
-            }
-          }
-        } else {
-          updatedGame.moves.push({
+    console.log(game.moves.length);
+    if (game.gameOver) {
+      updatedGame.comments = "Game is already over. Check the winner attribute";
+    } else {
+      if (game.currentState[0][req.params.columnNumber - 1] == 0) {
+        updatedGame.validMove = true;
+        updatedGame.comments =
+          "Valid move. Please check the currentState and moves attribute";
+        if (game.moves.length == 0) {
+          updatedGame.moves[0] = {
             color: "YELLOW",
-            moveCount: updatedGame.moves.length + 1,
+            moveCount: 1,
             columnNumber: req.params.columnNumber,
-          });
+          };
           for (let i = 5; i >= 0; i--) {
             if (game.currentState[i][req.params.columnNumber - 1] == 0) {
               updatedGame.currentState[i][req.params.columnNumber - 1] = "Y";
               break;
             }
           }
+        } else {
+          if ((updatedGame.moves.length + 1) % 2 == 0) {
+            updatedGame.moves.push({
+              color: "RED",
+              moveCount: updatedGame.moves.length + 1,
+              columnNumber: req.params.columnNumber,
+            });
+            for (let i = 5; i >= 0; i--) {
+              if (game.currentState[i][req.params.columnNumber - 1] == 0) {
+                updatedGame.currentState[i][req.params.columnNumber - 1] = "R";
+                break;
+              }
+            }
+          } else {
+            updatedGame.moves.push({
+              color: "YELLOW",
+              moveCount: updatedGame.moves.length + 1,
+              columnNumber: req.params.columnNumber,
+            });
+            for (let i = 5; i >= 0; i--) {
+              if (game.currentState[i][req.params.columnNumber - 1] == 0) {
+                updatedGame.currentState[i][req.params.columnNumber - 1] = "Y";
+                break;
+              }
+            }
+          }
         }
-      }
-      /* Logic for the checks */
-      const rows = await updatedGame.currentState.map((d) => {
-        return d.join("");
-      });
+        /* Logic for the checks */
+        const rows = await updatedGame.currentState.map((d) => {
+          return d.join("");
+        });
 
-      for (let i = 0; i < rows.length; i++) {
-        if (rows[i].includes("YYYY")) {
-          updatedGame.winner = "YELLOW";
-          updatedGame.gameOver = true;
-          break;
-        } else if (rows[i].includes("RRRR")) {
-          updatedGame.winner = "RED";
-          updatedGame.gameOver = true;
-          break;
+        for (let i = 0; i < rows.length; i++) {
+          if (rows[i].includes("YYYY")) {
+            updatedGame.winner = "YELLOW";
+            updatedGame.gameOver = true;
+            break;
+          } else if (rows[i].includes("RRRR")) {
+            updatedGame.winner = "RED";
+            updatedGame.gameOver = true;
+            break;
+          }
         }
-      }
-      const columns = ["", "", "", "", "", "", ""];
+        const columns = ["", "", "", "", "", "", ""];
 
-      for (let i = 0; i < 7; i++) {
-        for (let j = 0; j < 6; j++) {
-          columns[i] = columns[i].concat(rows[j].charAt(i));
+        for (let i = 0; i < 7; i++) {
+          for (let j = 0; j < 6; j++) {
+            columns[i] = columns[i].concat(rows[j].charAt(i));
+          }
         }
-      }
 
-      for (let i = 0; i < columns.length; i++) {
-        if (columns[i].includes("YYYY")) {
-          updatedGame.winner = "YELLOW";
-          updatedGame.gameOver = true;
-          break;
-        } else if (columns[i].includes("RRRR")) {
-          updatedGame.winner = "RED";
-          updatedGame.gameOver = true;
-          break;
+        for (let i = 0; i < columns.length; i++) {
+          if (columns[i].includes("YYYY")) {
+            updatedGame.winner = "YELLOW";
+            updatedGame.gameOver = true;
+            break;
+          } else if (columns[i].includes("RRRR")) {
+            updatedGame.winner = "RED";
+            updatedGame.gameOver = true;
+            break;
+          }
         }
+      } else {
+        updatedGame.validMove = false;
+        updatedGame.comments = "Invalid move. Please make a valid move";
       }
-    } else {
-      updatedGame.validMove = false;
-      updatedGame.comments = "Invalid move. Please make a valid move";
     }
-  }
-  //console.log(updatedGame);
+    //console.log(updatedGame);
 
-  Game.findByIdAndUpdate(req.params.id, updatedGame, { new: true }, function (
-    err,
-    resultGame
-  ) {
-    if (err) console.log(err);
-    res.send(resultGame);
-  });
+    Game.findByIdAndUpdate(req.params.id, updatedGame, { new: true }, function (
+      err,
+      resultGame
+    ) {
+      if (err) console.log(err);
+      res.send(resultGame);
+    });
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
