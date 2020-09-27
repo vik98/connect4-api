de117662dc9a3a0efa386ac98eb6b656e5bec9d7
Vikramsingh Rathod <rathodvikram44@gmail.com>
2020-09-27 23:13:46 +0530

    update logic

--- routes/index.js
+++ routes/index.js
@@ -41,7 +41,7 @@ router.post("/:id/:columnNumber", async function (req, res, next) {
         "Valid move. Please check the currentState and moves attribute";
       if (game.moves.length == 0) {
         updatedGame.moves[0] = {
-          color: "Yellow",
+          color: "YELLOW",
           moveCount: 1,
           columnNumber: req.params.columnNumber,
         };
@@ -54,7 +54,7 @@ router.post("/:id/:columnNumber", async function (req, res, next) {
       } else {
         if ((updatedGame.moves.length + 1) % 2 == 0) {
           updatedGame.moves.push({
-            color: "Red",
+            color: "RED",
             moveCount: updatedGame.moves.length + 1,
             columnNumber: req.params.columnNumber,
           });
@@ -66,7 +66,7 @@ router.post("/:id/:columnNumber", async function (req, res, next) {
           }
         } else {
           updatedGame.moves.push({
-            color: "Yellow",
+            color: "YELLOW",
             moveCount: updatedGame.moves.length + 1,
             columnNumber: req.params.columnNumber,
           });
@@ -78,12 +78,47 @@ router.post("/:id/:columnNumber", async function (req, res, next) {
           }
         }
       }
+      /* Logic for the checks */
+      const rows = await updatedGame.currentState.map((d) => {
+        return d.join("");
+      });
+
+      for (let i = 0; i < rows.length; i++) {
+        if (rows[i].includes("YYYY")) {
+          updatedGame.winner = "YELLOW";
+          updatedGame.gameOver = true;
+          break;
+        } else if (rows[i].includes("RRRR")) {
+          updatedGame.winner = "RED";
+          updatedGame.gameOver = true;
+          break;
+        }
+      }
+      const columns = ["", "", "", "", "", "", ""];
+
+      for (let i = 0; i < 7; i++) {
+        for (let j = 0; j < 6; j++) {
+          columns[i] = columns[i].concat(rows[j].charAt(i));
+        }
+      }
+
+      for (let i = 0; i < columns.length; i++) {
+        if (columns[i].includes("YYYY")) {
+          updatedGame.winner = "YELLOW";
+          updatedGame.gameOver = true;
+          break;
+        } else if (columns[i].includes("RRRR")) {
+          updatedGame.winner = "RED";
+          updatedGame.gameOver = true;
+          break;
+        }
+      }
     } else {
       updatedGame.validMove = false;
       updatedGame.comments = "Invalid move. Please make a valid move";
     }
   }
-  console.log(updatedGame);
+  //console.log(updatedGame);
 
   Game.findByIdAndUpdate(req.params.id, updatedGame, { new: true }, function (
     err,
